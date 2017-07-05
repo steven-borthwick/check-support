e1a54ad929b4382521ec34d58eade3a33c46b18d
steven-borthwick <stevenborthwick@hotmail.com>
2017-07-05 16:12:31 +0100

    Vnics version 4 and 5

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,8 @@ var routes4 = require('./routes/routes_4');
 var routes5 = require('./routes/routes_5');
 var routes6 = require('./routes/routes_6');
 var routes7 = require('./routes/routes_7');
+var routes8 = require('./routes/routes_8');
+var routes9 = require('./routes/routes_9');
 
 // Route index page
 router.use('/third_parties', routes1);
@@ -17,6 +19,9 @@ router.use('/third_partiesv3', routes3);
 router.use('/volnicsv1', routes4);
 router.use('/volnicsv2', routes5);
 router.use('/volnicsv3', routes6);
-router.use('/spa', routes6);
+router.use('/spa', routes7);
+router.use('/volnicsv4', routes8);
+router.use('/volnicsv5', routes9);
+
 
 module.exports = router
